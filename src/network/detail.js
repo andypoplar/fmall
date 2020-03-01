bdcff51cdc833d6581c6431c650c030b4d544fee
andypoplar <15532532890@163.com>
2020-03-01 16:19:43 +0800

    fmall

--- src/network/detail.js
+++ src/network/detail.js
@@ -3,7 +3,7 @@ import {
 } from './request';
 export function getDetail(iid) {
   return request({
-    url: '/api/hy/detail',
+    url: '/api/wh/detail',
     params: {
       iid
     }
@@ -12,7 +12,7 @@ export function getDetail(iid) {
 
 export function getRecommend(){
   return request({
-    url:'/api/hy/recommend'
+    url:'/api/wh/recommend'
   })
 }
 
--- src/network/home.js
+++ src/network/home.js
@@ -2,13 +2,13 @@ import {request} from './request';
 
 export function getHomeMultidata(){
   return request({
-    url:'/api/hy/home/multidata',
+    url:'/api/wh/home/multidata',
   })
 }
 
 export function getHomeGoods(type,page){
   return request({
-    url:'/api/hy/home/data',
+    url:'/api/wh/home/data',
     params:{
       type,
       page
